4b2d8b23e42e208c3ef6c3f897a68a794500d214
SkyNicky <nycholasffrocha@gmail.com>
2024-06-04 16:53:21 -0300

    teste1

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,9 +7,10 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: 'src/index.html'
+        main: 'index.html'
       }
     }
   },
   plugins: [react()],
-});
+})
+
