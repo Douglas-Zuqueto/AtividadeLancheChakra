cfabf591380e6cb4864aa54bcfa8fbc55132c702
SkyNicky <nycholasffrocha@gmail.com>
2024-06-03 21:57:12 -0300

    teste imagem unica

--- src/Componentes/dataMenu.js
+++ src/Componentes/dataMenu.js
@@ -20,49 +20,49 @@ export const responsive = {
 export const menuHamburguer = [
     {
       id: 1,
-      imageurl: "../assets/Img/teste.jpeg",
+      imageurl: "https://images.unsplash.com/photo-1586190848861-99aa4a171e90?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aGFtYnVndWVyfGVufDB8fDB8fHww",
       name: "Hambúrguer Clássico",
       price: "R$18,99"
   },
   {
       id: 2,
-      imageurl: "../assets/Img/teste.jpeg",
+      imageurl: "https://images.unsplash.com/photo-1586190848861-99aa4a171e90?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aGFtYnVndWVyfGVufDB8fDB8fHww",
       name: "Hambúrguer com 3 Bifes, Cheddar e Bacon",
       price: "R$25,99"
   },
   {
       id: 3,
-      imageurl: "../assets/Img/teste.jpeg",
+      imageurl: "https://images.unsplash.com/photo-1586190848861-99aa4a171e90?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aGFtYnVndWVyfGVufDB8fDB8fHww",
       name: "Hambúrguer Simples com Cebola Roxa",
       price: "R$15,99"
   },
   {
       id: 4,
-      imageurl: "../assets/Img/teste.jpeg",
+      imageurl: "https://images.unsplash.com/photo-1586190848861-99aa4a171e90?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aGFtYnVndWVyfGVufDB8fDB8fHww",
       name: "Hambúrguer com Dois Bifes, Cheddar e Picles",
       price: "R$22,50"
   },
   {
       id: 5,
-      imageurl: "../assets/Img/teste.jpeg",
+      imageurl: "https://images.unsplash.com/photo-1586190848861-99aa4a171e90?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aGFtYnVndWVyfGVufDB8fDB8fHww",
       name: "Hambúrguer Artesanal",
       price: "R$30,00"
   },
   {
       id: 6,
-      imageurl: "../assets/Img/teste.jpeg",
+      imageurl: "https://images.unsplash.com/photo-1586190848861-99aa4a171e90?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aGFtYnVndWVyfGVufDB8fDB8fHww",
       name: "Hambúrguer com Dois Bifes, Bacon e Cheddar",
       price: "R$28,50"
   },
   {
       id: 7,
-      imageurl: "../assets/Img/teste.jpeg",
+      imageurl: "https://images.unsplash.com/photo-1586190848861-99aa4a171e90?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aGFtYnVndWVyfGVufDB8fDB8fHww",
       name: "Hambúrguer Gourmet",
       price: "R$35,00"
   },
   {
       id: 8,
-      imageurl: "../assets/Img/teste.jpeg",
+      imageurl: "https://images.unsplash.com/photo-1586190848861-99aa4a171e90?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aGFtYnVndWVyfGVufDB8fDB8fHww",
       name: "Rosquinha de Hambúrguer com Bacon, Ovo e Cheddar",
       price: "R$20,00"
   }
@@ -70,49 +70,49 @@ export const menuHamburguer = [
 export const menuHotD = [
     {
       id: 1,
-      imageurl: "../assets/Img/teste.jpeg",
+      imageurl: "https://images.unsplash.com/photo-1586190848861-99aa4a171e90?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aGFtYnVndWVyfGVufDB8fDB8fHww",
       name: "Cachorro Quente com Bastante Molho",
       price: "R$12,99"
   },
   {
       id: 2,
-      imageurl: "../assets/Img/teste.jpeg",
+      imageurl: "https://images.unsplash.com/photo-1586190848861-99aa4a171e90?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aGFtYnVndWVyfGVufDB8fDB8fHww",
       name: "Cachorro Quente com Molho Vermelho",
       price: "R$13,99"
   },
   {
       id: 3,
-      imageurl: "../assets/Img/teste.jpeg",
+      imageurl: "https://images.unsplash.com/photo-1586190848861-99aa4a171e90?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aGFtYnVndWVyfGVufDB8fDB8fHww",
       name: "Cachorro Quente com Palmito",
       price: "R$14,99"
   },
   {
       id: 4,
-      imageurl: "../assets/Img/teste.jpeg",
+      imageurl: "https://images.unsplash.com/photo-1586190848861-99aa4a171e90?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aGFtYnVndWVyfGVufDB8fDB8fHww",
       name: "Cachorro Quente Simples",
       price: "R$10,99"
   },
   {
       id: 5,
-      imageurl: "../assets/Img/teste.jpeg",
+      imageurl: "https://images.unsplash.com/photo-1586190848861-99aa4a171e90?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aGFtYnVndWVyfGVufDB8fDB8fHww",
       name: "Calabresa",
       price: "R$11,99"
   },
   {
       id: 6,
-      imageurl: "../assets/Img/teste.jpeg",
+      imageurl: "https://images.unsplash.com/photo-1586190848861-99aa4a171e90?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aGFtYnVndWVyfGVufDB8fDB8fHww",
       name: "Cachorro Quente com Pedaços de Carne",
       price: "R$15,99"
   },
   {
       id: 7,
-      imageurl: "../assets/Img/teste.jpeg",
+      imageurl: "https://images.unsplash.com/photo-1586190848861-99aa4a171e90?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aGFtYnVndWVyfGVufDB8fDB8fHww",
       name: "Dogão",
       price: "R$16,99"
   },
   {
       id: 8,
-      imageurl: "../assets/Img/teste.jpeg",
+      imageurl: "https://images.unsplash.com/photo-1586190848861-99aa4a171e90?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aGFtYnVndWVyfGVufDB8fDB8fHww",
       name: "Cachorro Quente Insano",
       price: "R$18,99"
   }
@@ -120,49 +120,49 @@ export const menuHotD = [
   export const menuPorcao = [
     {
       id: 1,
-      imageurl: "../assets/Img/teste.jpeg",
+      imageurl: "https://images.unsplash.com/photo-1586190848861-99aa4a171e90?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aGFtYnVndWVyfGVufDB8fDB8fHww",
       name: "Aneis de Cebola",
       price: "R$18,99"
   },
   {
       id: 2,
-      imageurl: "../assets/Img/teste.jpeg",
+      imageurl: "https://images.unsplash.com/photo-1586190848861-99aa4a171e90?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aGFtYnVndWVyfGVufDB8fDB8fHww",
       name: "Mix",
       price: "R$23,50"
   },
   {
       id: 3,
-      imageurl: "../assets/Img/teste.jpeg",
+      imageurl: "https://images.unsplash.com/photo-1586190848861-99aa4a171e90?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aGFtYnVndWVyfGVufDB8fDB8fHww",
       name: "Camarão",
       price: "R$30,00"
   },
   {
       id: 4,
-      imageurl: "../assets/Img/teste.jpeg",
+      imageurl: "https://images.unsplash.com/photo-1586190848861-99aa4a171e90?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aGFtYnVndWVyfGVufDB8fDB8fHww",
       name: "Costelinha",
       price: "R$25,75"
   },
   {
       id: 5,
-      imageurl: "../assets/Img/teste.jpeg",
+      imageurl: "https://images.unsplash.com/photo-1586190848861-99aa4a171e90?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aGFtYnVndWVyfGVufDB8fDB8fHww",
       name: "Coxa de frango",
       price: "R$15,99"
   },
   {
       id: 6,
-      imageurl: "../assets/Img/teste.jpeg",
+      imageurl: "https://images.unsplash.com/photo-1586190848861-99aa4a171e90?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aGFtYnVndWVyfGVufDB8fDB8fHww",
       name: "Batata com bacon",
       price: "R$20,00"
   },
   {
       id: 7,
-      imageurl: "../assets/Img/teste.jpeg",
+      imageurl: "https://images.unsplash.com/photo-1586190848861-99aa4a171e90?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aGFtYnVndWVyfGVufDB8fDB8fHww",
       name: "File de peixe",
       price: "R$28,50"
   },
   {
       id: 8,
-      imageurl: "../assets/Img/teste.jpeg",
+      imageurl: "https://images.unsplash.com/photo-1586190848861-99aa4a171e90?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aGFtYnVndWVyfGVufDB8fDB8fHww",
       name: "Torresminho",
       price: "R$18,00"
   }
